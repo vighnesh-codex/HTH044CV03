6c944b8a412ced130bb7f2c7092c508aef40fbe0
vairavelv <vairavelv00@gmail.com>
2026-09-24 18:32:33 +0530

    added analyzer file

---
+++ Document_quality_gate/modules/analyzer.py
@@ -0,0 +1,25 @@
+from modules.blur import analyze_blur
+from modules.contrast import analyze_contrast
+from modules.skew import analyze_skew
+from modules.noise import analyze_noise
+from modules.handwriting import analyze_handwriting
+from modules.missing_section import analyze_missing_section
+
+
+def analyze_document(image):
+
+    blur = analyze_blur(image)
+    contrast = analyze_contrast(image)
+    skew = analyze_skew(image)
+    noise = analyze_noise(image)
+    handwriting = analyze_handwriting(image)
+    completeness = analyze_missing_section(image)
+
+    return {
+        "blur": blur,
+        "contrast": contrast,
+        "skew": skew,
+        "noise": noise,
+        "handwriting": handwriting,
+        "completeness": completeness
+    }
---
+++ Document_quality_gate/test_analyzer.py
@@ -0,0 +1,38 @@
+import cv2
+
+from modules.analyzer import analyze_document
+
+
+images = {
+    "Missing full Document": "Document_quality_gate/Datas/miss_full.png",
+    "Blur Document": "Document_quality_gate/Datas/blur.jpeg",
+    "Low Contrast": "Document_quality_gate/Datas/contrast.jpg",
+    "Noisy Document": "Document_quality_gate/Datas/noise.png",
+    "Skewed Document": "Document_quality_gate/Datas/skew.jpeg",
+    "Handwriting": "Document_quality_gate/Datas/handwrit.png",
+    "Missing Bottom": "Document_quality_gate/Datas/miss_bottom.png",
+    "Missing Middle + Bottom": "Document_quality_gate/Datas/miss_midbot.png",
+    "Missing Top": "Document_quality_gate/Datas/miss_top.png"
+}
+
+
+for name, path in images.items():
+
+    image = cv2.imread(path)
+
+    if image is None:
+        print(f"\nCould not load: {name}")
+        continue
+
+    result = analyze_document(image)
+
+    print("\n" + "=" * 50)
+    print(name)
+    print("=" * 50)
+
+    print("Blur:", result["blur"])
+    print("Contrast:", result["contrast"])
+    print("Skew:", result["skew"])
+    print("Noise:", result["noise"])
+    print("Handwriting:", result["handwriting"])
+    print("Completeness:", result["completeness"])
